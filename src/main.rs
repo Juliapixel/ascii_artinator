721042dc15e86f2b5184524d3b70676c336bacd3
Julia Borges <89038897+Juliapixel@users.noreply.github.com>
2023-07-29 01:41:19 -0300

    optimized zoazoGeneration ehehe

--- src/main.rs
+++ src/main.rs
@@ -87,19 +87,21 @@ fn generate_zoazo() -> String{
     let mut rng = rand::thread_rng();
     let zoazo_short = rng.gen_bool(0.5);
     let zoazo_len = if zoazo_short { 1 } else { rng.gen_range(2..=5) };
-    let mut zoazo_emote;
+    let mut zoazo_words: Vec<&str> = Vec::with_capacity(zoazo_len);
 
-    zoazo_emote = String::from("zoazo");
+    (0..zoazo_len).into_iter().for_each(|_| {zoazo_words.push(&WORD_LIST[rng.gen_range(0..WORD_LIST.len())])});
 
-    for _ in 0..zoazo_len {
-        let mut rand_word = WORD_LIST[rng.gen_range(0..WORD_LIST.len())].clone();
-        rand_word = rand_word
-            .chars()
-            .enumerate()
-            .map(|(i, c)| if i == 0 { return c.to_ascii_uppercase() } else { return c })
-            .collect::<String>();
-        zoazo_emote.push_str(&rand_word);
-    }
+    let mut zoazo_emote = String::with_capacity(5 + zoazo_words.iter().map(|w| w.len()).sum::<usize>());
+    zoazo_emote += "zoazo";
+    for w in zoazo_words.into_iter() {
+        w.chars().enumerate().for_each(|(i, c)| {
+            if i == 0 {
+                zoazo_emote.push(c.to_ascii_uppercase())
+            } else {
+                zoazo_emote.push(c)
+            }});
+    };
+
     return zoazo_emote;
 }
 
